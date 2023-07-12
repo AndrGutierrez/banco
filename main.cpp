6e7aee2e1433dfcbcf7ddd82827cc41355850b0e
JesusMoraColmenares48 <jesusmoracolmenares@gmail.com>
2023-07-11 22:47:05 -0400

    menu y opciones de agregar y pasar clientes agregadas

--- main.cpp
+++ main.cpp
@@ -1,32 +1,82 @@
 #include <iostream>
 #include <queue>
 #include "Utils.h"
+#include <cctype>
 using namespace std;
-void run(){
-	Utils utils;
-	queue<int> colaCedulas;
-	int opcion = 1;	
-	
-	while(opcion==1){
-		string cedula;
-		cout<<"Porfavor ingrese su cedula de identidad"<<endl;
-		getline(cin,cedula);	
-		do{			
-			cout<<"quiere ingresar otra cedula?"<<endl;
-			cout<<"1-si"<<endl;
-			cout<<"2-no"<<endl;			
-			cin>>opcion;
-			if(opcion>2){
-				cout<<"no ingresaste una opcion valida, intenta de nuevo"<<endl;
-				cout<<endl;
-			}	
-		}
-		while(opcion>2);
+
+void pasarClienteAtaquilla(queue<int> cola){		
+
 		
-		colaCedulas = utils.agregarCedula(colaCedulas, cedula);
-		cin.ignore();		
+}
+
+void run(){
+	char dato = 'F';
+	Utils util;
+	queue<int> colaCedulas;
+	while(dato != 'f' || dato != 'F'){
+		cout<<"OPCIONES:"<<endl;
+		cout<<"---- A: INGRESAR CEDULAS"<<endl;
+		cout<<"---- 1: LLAMAR AL SIGUIENTE DE LA COLA EN LA TAQUILLA 1"<<endl;
+		cout<<"---- 2: LLAMAR AL SIGUIENTE DE LA COLA EN LA TAQUILLA 2"<<endl;
+		cout<<"---- 3: LLAMAR AL SIGUIENTE DE LA COLA EN LA TAQUILLA 3"<<endl;	
+		cout<<"---- F: SALIR"<<endl;
+		cin>>dato;
+		cout<<endl;
+		cin.ignore();
+		
+		if(dato == 'A'){
+			colaCedulas = util.ingresarCedulas(colaCedulas);
+		}	
+		if(dato == '1'){
+			cout<<"TAQUILLA 1:"<<endl;
+			if(!colaCedulas.empty()){
+				colaCedulas = util.pasarClienteAtaquilla(colaCedulas);
+			}else{
+				cout<<"no hay mas clientes que atender"<<endl;
+			}			
+		}
+		if(dato == '2'){
+			cout<<"TAQUILLA 2:"<<endl;
+			if(!colaCedulas.empty()){
+				colaCedulas = util.pasarClienteAtaquilla(colaCedulas);
+			}else{
+				cout<<" no hay mas clientes que atender"<<endl;
+			}
+		}		
+		if(dato == '3'){
+			cout<<"TAQUILLA 3:"<<endl;
+			if(!colaCedulas.empty()){
+				colaCedulas = util.pasarClienteAtaquilla(colaCedulas);
+			}else{
+				cout<<"no hay mas clientes que atender"<<endl;
+			}
+		}	
+		/*
+		switch(dato){
+			
+			case 'A':
+				
+				break;
+			case '1':
+				cout<<"Taquilla 1"<<endl;
+				util.pasarClienteAtaquilla(colaCedulas);
+				break;					
+			case '2':
+				cout<<"Taquilla 2"<<endl;
+				util.pasarClienteAtaquilla(colaCedulas);
+				break;					
+			case '3':
+				cout<<"Taquilla 3"<<endl;
+				util.pasarClienteAtaquilla(colaCedulas);
+				break;													
+		}	
+		*/
+
 	}
-	utils.imprimirCola(colaCedulas);
+
+	int opcion = 1;	
+
+	
 }
 		
 	
