47b2814595e3d36231c6c6ebb5f16b180306f1f8
JesusMoraColmenares48 <jesusmoracolmenares@gmail.com>
2023-07-11 23:33:11 -0400

    pilas para invertir

--- Utils.h
+++ Utils.h
@@ -2,25 +2,89 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cstring>
 using namespace std;
+
+
+class Stack {
+public:
+    int top;
+    unsigned capacity;
+    char* array;
+};
+
+Stack* createStack(unsigned capacity)
+{
+    Stack* stack = new Stack();
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array
+        = new char[(stack->capacity * sizeof(char))];
+    return stack;
+}
+
+int isFull(Stack* stack)
+{
+    return stack->top == stack->capacity - 1;
+}
+
+int isEmpty(Stack* stack) { return stack->top == -1; }
+
+void push(Stack* stack, char item)
+{
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+}
+
+char pop(Stack* stack)
+{
+    if (isEmpty(stack))
+        return -1;
+    return stack->array[stack->top--];
+}
+
+void reverse(char str[])
+{
+
+    int n = strlen(str);
+    Stack* stack = createStack(n);
+
+    int i;
+    for (i = 0; i < n; i++)
+        push(stack, str[i]);
+
+    for (i = 0; i < n; i++)
+        str[i] = pop(stack);
+}
+
 
 class Utils{
 public:
 	Utils(){}
+	
+		
 	queue<int> agregarCedula(queue<int> cola, string cedula){
 		int cantidadDigitos=3;
 		int i=cedula.length()-cantidadDigitos;
 		bool cedulaYaAgregada = false;
 		int digitos;
+		char DigitosChar[4];
 
 		do{
 			digitos=obtenerTresDigitos(i, cedula, 3);
 			cedulaYaAgregada=existeEnCola(cola, digitos);
+			
 			i--;
 		}
 		while(cedulaYaAgregada && i>=0);
 		if(i<0 && cedulaYaAgregada){
-			reverse(cedula.begin(), cedula.end()); 
+			
+			
+			memset(DigitosChar, 0, sizeof(DigitosChar));
+			snprintf(DigitosChar, sizeof(DigitosChar),"%03d", digitos);
+			cout<<"TRES DIGITOS"<<DigitosChar;
+			reverse(DigitosChar);
 			i=cedula.length()-cantidadDigitos;
 			do{
 				digitos=obtenerTresDigitos(i, cedula, 3);
