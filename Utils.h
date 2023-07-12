9991e7514cd336145f7f68fe712bec9a7c8f2f2d
JesusMoraColmenares48 <jesusmoracolmenares@gmail.com>
2023-07-11 21:46:12 -0400

    agregando menu

--- Utils.h
+++ Utils.h
@@ -66,6 +66,23 @@ public:
 			copiaCola.pop();
 		}
 	}
+	
+	
+	void ingresarCedulas(queue<int> colaCedulas){
+		string cedula;
+		cout<<"Porfavor ingrese su cedula de identidad"<<endl;
+		getline(cin,cedula);			
+		colaCedulas = agregarCedula(colaCedulas, cedula);
+				
+	}
+	
+	void pasarClienteAtaquilla(queue<int> cola){
+		
+		cout<<"la clave del cliente que sigue en la cola virtual es " <<cola.front()<<endl;
+		cout<<"el cliente con clave " << cola.front() << " ha sido atendido";
+		cola.pop();
+		
+	}
 
 };
 
