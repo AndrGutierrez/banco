d03133b861f60ab321627e8e55121a9d90584701
JesusMoraColmenares48 <jesusmoracolmenares@gmail.com>
2023-07-11 20:05:01 -0400

    ingresar varias cedulas

--- main.cpp
+++ main.cpp
@@ -5,11 +5,33 @@ using namespace std;
 void run(){
 	Utils utils;
 	queue<int> colaCedulas;
-	colaCedulas = utils.agregarCedula(colaCedulas, "123456789");
+	int opcion = 1;	
+	
+	while(opcion==1){
+		string cedula;
+		cout<<"Porfavor ingrese su cedula de identidad"<<endl;
+		getline(cin,cedula);	
+		do{			
+			cout<<"quiere ingresar otra cedula?"<<endl;
+			cout<<"1-si"<<endl;
+			cout<<"2-no"<<endl;			
+			cin>>opcion;
+			if(opcion>2){
+				cout<<"no ingresaste una opcion valida, intenta de nuevo"<<endl;
+				cout<<endl;
+			}	
+		}
+		while(opcion>2);
+		
+		colaCedulas = utils.agregarCedula(colaCedulas, cedula);
+		cin.ignore();		
+	}
 	utils.imprimirCola(colaCedulas);
 }
+		
+	
+		
 int main(){
 	run();
-
 	return 0;
 }
